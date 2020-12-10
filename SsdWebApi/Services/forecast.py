2c89369aed4146a1af6c4a2a07a123a82888d2d7
Gioele Masini <gioele.masini@maggioli.it>
2020-12-10 22:07:10 +0100

    Uniforma mlp e forecast

--- SsdWebApi/Services/forecast.py
+++ SsdWebApi/Services/forecast.py
@@ -1,6 +1,6 @@
 import numpy as np, pandas as pd
 import matplotlib.pyplot as plt
-import os, sys, io, base64, math
+import os, sys, io, base64
 
 # Startup config
 np.random.seed(550) # for reproducibility
@@ -21,22 +21,15 @@ def print_figure(fig):
 	fig.savefig(buf, format='png')
 	print(base64.b64encode(buf.getbuffer()))
 
-# from series of values to windows matrix
-def compute_windows(nparray, npast=1):
-  dataX, dataY = [], [] # window and value
-  for i in range(len(nparray)-npast-1):
-    a = nparray[i:(i+npast), 0]
-    dataX.append(a)
-    dataY.append(nparray[i + npast, 0])
-  return np.array(dataX), np.array(dataY)
-
 def plot(forecastResult, shouldShowPlot):
-  plt.plot(forecastResult.dataset)
-  plt.plot(np.concatenate((np.full(1,np.nan),forecastResult.train[:,0])))
-  plt.plot(np.concatenate((np.full(len(forecastResult.dataset)-len(forecastResult.train)+1,np.nan), forecastResult.test[:,0])))
-  plt.plot(np.concatenate((np.full(len(forecastResult.dataset)+1,np.nan), forecastResult.forecast[:,0])))
+  plt.plot(forecastResult.dataset['value'], 'black', label = 'History')
+  plt.plot(forecastResult.train,label='Train')
+  plt.plot([None for x in forecastResult.train]+[x for x in forecastResult.test], label='Test')
+  plt.plot([None for x in forecastResult.train]+[None for x in forecastResult.test]+[x for x in forecastResult.forecast], label='Forecast')
   plt.xlabel('time')
   plt.ylabel('value')
+  plt.title('Index', color='black')
+  plt.legend()
 
   if (shouldShowPlot == True):
     plt.show()
@@ -44,7 +37,6 @@ def plot(forecastResult, shouldShowPlot):
     print_figure(plt.gcf())
 
 def autocorrelation(df, months, shouldShowPlot):
-  import pmdarima as pm # pip install pmdarima
   aValues = df['value'].to_numpy() # array of data
   logdata = np.log(aValues) # log transform
   data = pd.Series(logdata) # convert to pandas series
@@ -56,131 +48,115 @@ def autocorrelation(df, months, shouldShowPlot):
   sm.graphics.tsa.plot_acf(data.values, lags=2000)
   plt.show()
 
-def sarima(df, months, shouldShowPlot):
-  import pmdarima as pm # pip install pmdarima
+def mlp(df, months, shouldShowPlot):
+  import pandas as pd, numpy as np
+
   aValues = df['value'].to_numpy() # array of data
   logdata = np.log(aValues) # log transform
-  data = pd.Series(logdata) # convert to pandas series
+  data = pd.Series(logdata).diff() # convert to pandas series and diff transform
 
+  # Preprocessed data
   # plt.rcParams["figure.figsize"] = (10,8) # redefines figure size
-  plt.plot(data.values)
+  plt.plot(data.values);
   plt.show() # data plot
 
+  from statsmodels.graphics.tsaplots import plot_acf
 
-  # acf plot, industrial
-  # analisi dei dati con diagramma di autocorrelazione
-  import statsmodels.api as sm
-  # lags = valori su cui calcolare l'autocorrelazione
-  sm.graphics.tsa.plot_acf(data.values, lags=300)
+  # Original Series
+  plt.rcParams.update({'figure.figsize':(9,7), 'figure.dpi':120})
+  fig, axes = plt.subplots(2, 2, sharex=True)
+  axes[0, 0].plot(df['value']);
+  axes[0, 0].set_title('Original Series')
+  plot_acf(df['value'], ax=axes[0, 1], lags=2000)
+
+  # 1st Differencing
+  axes[1, 0].plot(data);
+  axes[1, 0].set_title('1st Order Differencing')
+  plot_acf(data.dropna(), ax=axes[1, 1], lags=2000)
   plt.show()
 
-  # train and test set
+  # train and test setm
   train = data[:-200]
   test = data[-200:]
+  train[0] = 0
 
-  # simple reconstruction, not necessary, unused
-  # reconstruct = np.exp(np.r_[train,test])
+  plt.plot(train.values);
+  plt.plot([None for x in train]+test.values.tolist());
+  plt.show() # data plot
+
+  # reconstruct = np.exp(np.r_[train,test]) # simple recosntruction
+
+  # ------------------------------------------------- neural forecast
+  from keras.preprocessing.sequence import TimeseriesGenerator
+  n_input = 5
+  generator = TimeseriesGenerator(train.values, train.values, length=n_input, batch_size=1)
+
+  from keras.models import Sequential
+  from keras.layers import Dense
+
+  model = Sequential()
+  # lstm_model.add(Dense(30, activation='linear', input_dim=n_input))
+  model.add(Dense(n_input, activation='relu', input_dim=n_input))
+  model.add(Dense(1))
+  model.compile(optimizer='adam', loss='mse')
+  model.fit(generator,epochs=3) #epochs=25
+  model.summary()
+
+  # Andamento loss
+  # Ogni volta lavoro su un sottoinsieme di dati quindi può peggiorare
+  # losses_lstm = model.history.history['loss']
+  # plt.xticks(np.arange(0,21,1)) # convergence trace
+  # plt.plot(range(len(losses_lstm)),losses_lstm);
+  # plt.show()
+
+  # Prediction
+  lstm_forecast = list()
+  batch = train.values[-n_input:]  # Metto dentro gli ultimi dati che userò come input per il primo valore previsto
+  curbatch = batch.reshape((1, n_input))  # Creo l'array a partire dalla struttura keras
+  for i in range(len(test)):
+    lstm_pred = model.predict(curbatch)[0]
+    lstm_forecast.append(lstm_pred) # Salvo il valore previsto
+    curbatch = np.append(curbatch[:,1:],[lstm_pred], axis=1) # Rimuovo il valore più vecchio e aggiungo quello appena previsto
+
+  yfore = np.transpose(lstm_forecast).squeeze() # Transpose: da array verticale lo faccio diventare un array orizzontale/"normale"
 
 
-  # auto arima (calcolo dei parametri con una ricerca nello spazio)
-  # SARIMA(p,d,q)(P,D,Q,m)
-  model = pm.auto_arima(
-    train.values, test='adf',
-    start_p=1, start_q=1,
-    max_p=3, max_q=3, d=None,
-    #m=5, D=1, start_P=0,
-    #p=2, d=0, q=1,
-    seasonal=False,
-    trace=True,
-    error_action='ignore',
-    suppress_warnings=True,
-    stepwise=True
-    ) # False full grid
+  # evaluate the keras model
+  # accuracy = model.evaluate(generator)
+  # print('Accuracy: %.2f' % (accuracy*100))
 
-  print(model.summary())
-  morder = model.order; print("Sarimax order {0}".format(morder))
-  mseasorder = model.seasonal_order;
-  print("Sarimax seasonal order {0}".format(mseasorder))
+  # Forecast
+  lstm_forecast_2 = list()
+  batch = np.array(lstm_forecast)[-n_input:]  # Metto dentro gli ultimi dati che userò come input per il primo valore previsto
+  curbatch = batch.reshape((1, n_input))  # Creo l'array a partire dalla struttura keras
+  for i in range(len(test)):
+    lstm_pred = model.predict(curbatch)[0]
+    lstm_forecast_2.append(lstm_pred) # Salvo il valore previsto
+    curbatch = np.append(curbatch[:,1:],[lstm_pred],axis=1) # Rimuovo il valore più vecchio e aggiungo quello appena previsto
 
-  # predictions and forecasts
-  fitted = model.fit(train)
-  ypred = fitted.predict_in_sample() # prediction (in-sample)
-  yfore = fitted.predict(n_periods=12) # forecast (out-of-sample)
-  plt.plot(train.values)
-  plt.plot([None for x in range(12)]+[x for x in ypred[12:]])
-  plt.plot([None for x in ypred]+[x for x in yfore])
-  plt.xlabel('time');plt.ylabel('value')
-  plt.show()
+  zfore = np.transpose(lstm_forecast_2).squeeze() # Transpose: da array verticale lo faccio diventare un array orizzontale/"normale"
 
   # recostruction
-  yplog = pd.Series(ypred)
-  expdata = np.exp(yplog) # unlog
-  expfore = np.exp(yfore)
-  plt.plot([None for x in range(12)]+[x for x in expdata[12:]])
-  plt.plot(df["value"])
-  plt.plot([None for x in expdata]+[x for x in expfore])
-  plt.show()
+  train[0] = 0
+  exptrain = np.exp(train.cumsum()+logdata[0]) # unlog
+  #yfore[0] = 0
+  exptest = np.exp(yfore.cumsum()+logdata[len(logdata)-201])
+  expfore = np.exp(zfore.cumsum()+logdata[len(logdata)-1])
+  #expfore = np.exp(zfore)
 
-def mlp(df, months, shouldShowPlot):
-
-  # Keras astrae i modelli e le funzionalità rispetto alle diverse librerie di reti neurali
-  from keras.models import Sequential # Percettone multilivello
-  from keras.layers import Dense # Livelli con connessioni dense
-
-  dataset = df.values # time series values
-  dataset = dataset.astype('float32') # needed for MLP input (richiesto da tensorflow)
-
-  # train - test sets
-  cutpoint = int(len(dataset) * 0.7) # 70% train, 30% test
-  train, test = dataset[:cutpoint], dataset[cutpoint:]
-  print("MAPE Len train={0}, len test={1}".format(len(train), len(test)))
-
-  # sliding window matrices (npast = window width); dim = n - npast - 1
-  npast = 7
-  # Y = valore desiderato in uscita
-  trainX, trainY = compute_windows(train, npast)
-  testX, testY = compute_windows(test, npast) # should get also the last npred of train
-
-  # Multilayer Perceptron model
-  model = Sequential()
-  n_hidden = 8
-  n_output = 1
-  model.add(Dense(n_hidden, input_dim=npast, activation='relu')) # hidden neurons, add 1 layer
-  model.add(Dense(n_output)) # output neurons
-  model.compile(loss='mean_squared_error', optimizer='adam') # loss = errore quadratico medio, adam = aggiustamento dei pesi
-  model.fit(trainX, trainY, epochs=200, batch_size=10, verbose=2) # batch_size len(trainX)
-
-
-  # Model performance
-  trainScore = model.evaluate(trainX, trainY, verbose=0)
-  print('MAPE Score on train: MSE = {0:0.2f} '.format(trainScore))
-  testScore = model.evaluate(testX, testY, verbose=0)
-  print('MAPE Score on test: MSE = {0:0.2f} '.format(testScore))
-
-  trainPredict = model.predict(trainX) # predictions
-  testForecast = model.predict(testX) # forecast
-
-  #return ForecastResult(dataset, trainPredict, testForecast, )
-  # plt.rcParams["figure.figsize"] = (10,8) # redefines figure size
-  plt.plot(dataset)
-  plt.plot(np.concatenate((np.full(1,np.nan),trainPredict[:,0])))
-  plt.plot(np.concatenate((np.full(len(train)+1,np.nan), testForecast[:,0])))
-  plt.xlabel('time');plt.ylabel('value')
-
-  if (shouldShowPlot == True):
-    plt.show()
-  print_figure(plt.gcf())
+  plot(ForecastResult(df, exptrain, exptest, expfore), True)
+  return ForecastResult(df, exptrain, exptest, expfore)
 
 def lstm(df, months, shouldShowPlot):
-  import pandas as pd, numpy as np, os
+  import pandas as pd, numpy as np
   import matplotlib.pyplot as plt
 
-  df.set_index('period')
   aValues = df['value'].to_numpy() # array of data
   logdata = np.log(aValues) # log transform
   data = pd.Series(logdata) # convert to pandas series
 
-  plt.rcParams["figure.figsize"] = (10,8) # redefines figure size
+  # Preprocessed data
+  # plt.rcParams["figure.figsize"] = (10,8) # redefines figure size
   plt.plot(data.values);
   plt.show() # data plot
 
@@ -188,7 +164,7 @@ def lstm(df, months, shouldShowPlot):
   train = data[:-200]
   test = data[-200:]
 
-  reconstruct = np.exp(np.r_[train,test]) # simple recosntruction
+  # reconstruct = np.exp(np.r_[train,test]) # simple recosntruction
 
   # ------------------------------------------------- neural forecast
   from sklearn.preprocessing import MinMaxScaler
@@ -198,7 +174,7 @@ def lstm(df, months, shouldShowPlot):
   scaled_test_data = scaler.transform(test.values.reshape(-1, 1))
 
   from keras.preprocessing.sequence import TimeseriesGenerator
-  n_input = 12; n_features = 1
+  n_input = 10; n_features = 1
   generator = TimeseriesGenerator(scaled_train_data, scaled_train_data, length=n_input,
   batch_size=1)
 
@@ -207,36 +183,51 @@ def lstm(df, months, shouldShowPlot):
   from keras.layers import LSTM
 
   lstm_model = Sequential()
-  lstm_model.add(LSTM(20, activation='relu', input_shape=(n_input, n_features), dropout=0.05))
+  lstm_model.add(LSTM(10, activation='relu', input_shape=(n_input, n_features), dropout=0.05))
   lstm_model.add(Dense(1))
   lstm_model.compile(optimizer='adam', loss='mse')
   lstm_model.summary()
-  lstm_model.fit_generator(generator,epochs=25)
+  lstm_model.fit_generator(generator,epochs=3) #epochs=25
 
+  # Andamento loss
+  # Ogni volta lavoro su un sottoinsieme di dati quindi può peggiorare
   losses_lstm = lstm_model.history.history['loss']
   plt.xticks(np.arange(0,21,1)) # convergence trace
   plt.plot(range(len(losses_lstm)),losses_lstm);
   plt.show()
 
+  # Prediction
   lstm_predictions_scaled = list()
-  batch = scaled_train_data[-n_input:]
-  curbatch = batch.reshape((1, n_input, n_features))
+  batch = scaled_train_data[-n_input:]  # Metto dentro gli ultimi dati che userò come input per il primo valore previsto
+  curbatch = batch.reshape((1, n_input, n_features))  # Creo l'array a partire dalla struttura keras
   for i in range(len(test)):
     lstm_pred = lstm_model.predict(curbatch)[0]
-    lstm_predictions_scaled.append(lstm_pred)
-    curbatch = np.append(curbatch[:,1:,:],[[lstm_pred]],axis=1)
+    lstm_predictions_scaled.append(lstm_pred) # Salvo il valore previsto
+    curbatch = np.append(curbatch[:,1:,:],[[lstm_pred]],axis=1) # Rimuovo il valore più vecchio e aggiungo quello appena previsto
 
-  lstm_forecast = scaler.inverse_transform(lstm_predictions_scaled)
-  yfore = np.transpose(lstm_forecast).squeeze()
+  lstm_forecast = scaler.inverse_transform(lstm_predictions_scaled) # Lo scaler si ricorda la funzione usata per scalare e ricalcola i valori
+  yfore = np.transpose(lstm_forecast).squeeze() # Transpose: da array verticale lo faccio diventare un array orizzontale/"normale"
+
+  # Forecast
+  # lstm_forecast_scaled = list()
+  # batch = np.array(lstm_predictions_scaled)[-n_input:]  # Metto dentro gli ultimi dati che userò come input per il primo valore previsto
+  # curbatch = batch.reshape((1, n_input, n_features))  # Creo l'array a partire dalla struttura keras
+  # for i in range(len(test)):
+  #   lstm_pred = lstm_model.predict(curbatch)[0]
+  #   lstm_forecast_scaled.append(lstm_pred) # Salvo il valore previsto
+  #   curbatch = np.append(curbatch[:,1:,:],[[lstm_pred]],axis=1) # Rimuovo il valore più vecchio e aggiungo quello appena previsto
+
+  # lstm_forecast_2 = scaler.inverse_transform(lstm_forecast_scaled) # Lo scaler si ricorda la funzione usata per scalare e ricalcola i valori
+  # zfore = np.transpose(lstm_forecast_2).squeeze() # Transpose: da array verticale lo faccio diventare un array orizzontale/"normale"
 
   # recostruction
-  expdata = np.exp(train) # unlog
-  expfore = np.exp(yfore)
-  plt.plot(df["value"], label="values")
-  plt.plot(expdata,label='expdata')
-  plt.plot([None for x in expdata]+[x for x in expfore], label='forecast')
-  plt.legend()
-  plt.show()
+  exptrain = np.exp(train) # unlog
+  exptest = np.exp(yfore)
+  expfore = exptest
+  #expfore = np.exp(zfore)
+
+  plot(ForecastResult(df, exptrain, exptest, expfore), True)
+  return ForecastResult(df, exptrain, exptest, expfore)
 
 if __name__ == "__main__":
   # change working directory to script path
@@ -255,16 +246,23 @@ if __name__ == "__main__":
 
   # Add date to df
   df_date = pd.read_csv("../Data.csv", usecols=[0], names=['date'], header=0)
-  #df['period'] = pd.to_datetime(df_date['date'], format="%Y-%m-%d").dt.to_period('D')
+  df['period'] = pd.to_datetime(df_date['date'], format="%Y-%m-%d").dt.to_period('D')
+  df.set_index('period')
 
-  if tec == "sarima":
-    sarima(df, 12, shouldShowPlot)
-  elif tec == "autocorrelation":
-    autocorrelation(df, 12, shouldShowPlot)
-  elif tec == "lstm":
+  #lstm(df, 12, shouldShowPlot)
+  if tec == "lstm":
     lstm(df, 12, shouldShowPlot)
   else:
     mlp(df, 12, shouldShowPlot)
+
+  #if tec == "sarima":
+    #sarima(df, 12, shouldShowPlot)
+  #elif tec == "autocorrelation":
+    #autocorrelation(df, 12, shouldShowPlot)
+  #elif tec == "lstm":
+    #lstm(df, 12, shouldShowPlot)
+  #else:
+    #mlp(df, 12, shouldShowPlot)
 
   #plt.plot(df)
   #if (shouldShowPlot == True):
